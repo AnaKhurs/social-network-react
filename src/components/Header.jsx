56fb190c3afc8c41d69b640c80594d9490600325
AnaKhurs <ana.khurs@gmail.com>
2021-08-09 01:39:15 +0300

    New app

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://pbs.twimg.com/profile_images/1063925348205821958/DlGcxdOl_400x400.jpg'/>
+        </header>);
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://lh3.googleusercontent.com/proxy/-3cUPl5BFg1SidVXwFoAKtNituN9LBjcHnxUOO0vb2yd6ofGVxJNfDUhHPTxn26pTEi6Q13ne2WuDQbeNwSEm4ya'/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div> My posts
+                <div>New post</div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>);
+}
+export default Profile;
