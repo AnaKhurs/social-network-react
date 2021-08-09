b448fbd65433aba9f5c6223d3f1ecc0fd88d051a
AnaKhurs <ana.khurs@gmail.com>
2021-08-09 22:40:27 +0300

    Add page Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,21 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-       <div>
-           Dialogs
+       <div className={classes.dialogs}>
+           <div className={classes.dialogsItem}>
+               <div className={`${classes.dialog} ${classes.active}`}>Nastya</div>
+               <div className={classes.dialog}>Maks</div>
+               <div className={classes.dialog}>Lena</div>
+               <div className={classes.dialog}>Masha</div>
+               <div className={classes.dialog}>Sasha</div>
+               <div className={classes.dialog}>Alex</div>
+               <div className={classes.dialog}>Pasha</div>
+           </div>
+           <div className={classes.messages}>
+               <div className={classes.message}>Hi</div>
+               <div className={classes.message}>How are you?</div>
+               <div className={classes.message}>Yo!</div>
+           </div>
        </div>
     );
 }
