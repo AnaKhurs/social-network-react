496c0d22fde7bc77579337934cda61f67c1ced11
AnaKhurs <ana.khurs@gmail.com>
2021-08-09 20:42:34 +0300

    route, browser-router

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div>
             <div>
                 <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
             </div>
