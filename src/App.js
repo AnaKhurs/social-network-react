f6c15279f7e7e12fb8c62ec9275127d83dcf58b2
AnaKhurs <ana.khurs@gmail.com>
2021-08-10 23:44:26 +0300

    data storage dialogs

--- src/App.js
+++ src/App.js
@@ -16,6 +16,22 @@ let postData = [
     {id: 2, message: 'It is my new post!', likecounter: 18}
 ]
 
+let dialogsData = [
+    {id: 1, name: 'Nastya'},
+    {id: 2, name: 'Lena'},
+    {id: 3, name: 'Masha'},
+    {id: 4, name: 'Sasha'},
+    {id: 5, name: 'Alex'},
+    {id: 6, name: 'Maks'},
+    {id: 7, name: 'Pasha'}
+]
+
+let messageData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you?'},
+    {id: 3, message: 'Yo!'}
+]
+
 
 const App = () => {
     return (
@@ -24,7 +40,7 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={() => <Dialogs/>}/>
+                    <Route path='/dialogs' render={() => <Dialogs dialogsData={dialogsData} messageData={messageData}/>}/>
                     <Route path='/profile' render={() => <Profile postData={postData}/>}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/news' component={News}/>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,28 +5,12 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
-        {id: 1, name: 'Nastya'},
-        {id: 2, name: 'Lena'},
-        {id: 3, name: 'Masha'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Alex'},
-        {id: 6, name: 'Maks'},
-        {id: 7, name: 'Pasha'}
-    ]
-
-    let messageData = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Yo!'}
-    ]
-
-    let dialogsElements = dialogsData
-        .map((d) => <DialogItem name={d.name} id={d.id}/>
+    let dialogsElements =
+        props.dialogsData.map((d) => <DialogItem name={d.name} id={d.id}/>
         )
 
-    let messagesElements = messageData
-        .map((m) => <Message message={m.message}/>
+    let messagesElements =
+        props.messageData.map((m) => <Message message={m.message}/>
         )
 
     return (
