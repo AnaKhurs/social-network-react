bab1ca8354f8632630fc1aececc90a08e29da1d3
AnaKhurs <ana.khurs@gmail.com>
2021-08-11 19:48:53 +0300

    UI dialog

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import classes from './../Dialogs.module.css'
+import classes from './Message.module.css'
 
 const Message = (props) => {
     return (
-        <div className={classes.message}>{props.message}</div>
+        <div className={classes.message}>
+            {props.message}
+</div>
     );
 }
 
